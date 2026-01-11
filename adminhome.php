8562a84d63d4127082a1b57c72a835357672d5b3
ayesha200352 <ayeshasenuri766@gmail.com>
2026-01-11 12:40:36 +0530

    Improve UI

--- adminhome.php
+++ adminhome.php
@@ -13,7 +13,15 @@ if (isset($_SESSION['admin'])) {
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 
-	
+	<body>
+		<header>
+			<div class="container">
+				<h1>quiz-db</h1>
+				<a href="index.php" class="start">Home</a>
+				<a href="add.php" class="start">Add Question</a>
+				<a href="allquestions.php" class="start">All Questions</a>
+				<a href="players.php" class="start">Players</a>
+				<a href="exit.php" class="start">Logout</a>
 
 			</div>
 		</header>
